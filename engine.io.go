aa16c83feca6e5b8eb185b891a3085fa16bae68d
zishang520 <zishang520@gmail.com>
2019-11-26 15:55:06 +0800

    优化

--- engine.io.go
+++ engine.io.go
@@ -1,24 +1,19 @@
 package engineio
 
 var (
-	Protocol int = 1
+	Protocol int64 = 1
 
-	attach = func(server, options) {
+	Attach = func(server, options) {
 		engine := NewServer(options)
-		engine.attach(server, options)
+		engine.Attach(server, options)
 		return engine
 	}
 
 	Listen = func(port, options, fn) {
-		if `function` == options {
-			fn = options
-			// options = {};
-		}
 		server := http.createServer(func(req, res) {
 			res.writeHead(501)
 			res.end(`Not Implemented`)
 		})
-
 		// create engine server
 		engine := Attach(server, options)
 		engine.httpServer = server

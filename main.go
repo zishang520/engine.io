f404b723a95dafa6956cebd16230d4c12abb7e5f
zishang520 <zishang520@gmail.com>
2019-11-28 18:43:19 +0800

    完成encodepacket

--- main.go
+++ main.go
@@ -6,32 +6,38 @@ import (
 	"github.com/zishang520/engine.io/parser"
 	"github.com/zishang520/engine.io/types"
 	"strings"
+	"unicode/utf8"
 )
 
 func main() {
-	buf, err := parser.EncodePacket(types.Packet{
+	buf, err := parser.EncodePacket(&types.Packet{
 		Type: "ping",
 		Data: bytes.NewBuffer([]byte{0, 1, 2, 3, 4, 5, 6, 7, 8, 9}),
 	}, false, false)
-	bufs, err := parser.EncodePacket(types.Packet{
+	bufs, err := parser.EncodePacket(&types.Packet{
 		Type: "ping",
 		Data: bytes.NewBuffer([]byte{0, 1, 2, 3, 4, 5, 6, 7, 8, 9}),
 	}, true, false)
-	bufss, err := parser.EncodePacket(types.Packet{
+	bufss, err := parser.EncodePacket(&types.Packet{
 		Type: "ping",
 		Data: strings.NewReader(`[]byte{0, 1你好呀, 2, 3, 4, 5, 6, 7, 8, 9}`),
 	}, true, false)
-	bufsss, err := parser.EncodePacket(types.Packet{
+	bufsss, err := parser.EncodePacket(&types.Packet{
 		Type: "ping",
 		Data: strings.NewReader(`[]byte{0, 1, 2, 3, 你好呀4, 5, 6, 7, 8, 9}`),
 	}, true, true)
 	fmt.Println(buf)
 	fmt.Println(bufs)
+
 	fmt.Println(bufss)
 	fmt.Println(bufsss)
 	fmt.Println(err)
 	fmt.Println(byte(0) + '0')
 	fmt.Println('b')
 	fmt.Println(0x31)
-	fmt.Println(parser.EMPTY_BUFFER)
+	xxxx := []byte{0xFF}
+	b, l := utf8.DecodeRuneInString(`b[]byte{0, 1你好呀, 2, 3, 4, 5, 6, 7, 8, 9}`)
+	fmt.Println(byte(b) == 'b')
+	fmt.Println(xxxx)
+	fmt.Println(l)
 }
--- parser/parser.go
+++ parser/parser.go
@@ -29,6 +29,8 @@ var (
 	packetslist map[byte]string = map[byte]string{0x30: "open", 0x31: "close", 0x32: "ping", 0x33: "pong", 0x34: "message", 0x35: "upgrade", 0x36: "noop"}
 
 	EMPTY_BUFFER *bytes.Buffer = new(bytes.Buffer)
+
+	err = types.Packet{Type: `error`, Data: strings.NewReader(`parser error`)}
 )
 
 /**
@@ -47,15 +49,8 @@ var (
  * @api public
  */
 
-func EncodePacket(packet types.Packet, supportsBinary bool, utf8encode bool) (*bytes.Buffer, error) {
+func EncodePacket(packet *types.Packet, supportsBinary bool, utf8encode bool) (*bytes.Buffer, error) {
 	encode := bytes.NewBuffer(nil)
-
-	if !supportsBinary {
-		encode.Write([]byte{'b', packets[packet.Type]})
-	} else {
-		encode.WriteByte(packets[packet.Type])
-	}
-	var dataByte []byte
 
 	if c, ok := packet.Data.(io.Closer); ok {
 		defer c.Close()
@@ -63,29 +58,37 @@ func EncodePacket(packet types.Packet, supportsBinary bool, utf8encode bool) (*b
 
 	switch v := packet.Data.(type) {
 	case *bytes.Buffer:
-		dataByte = v.Bytes()
+		if !supportsBinary {
+			encode.Write([]byte{'b', packets[packet.Type]})
+		} else {
+			encode.WriteByte(packets[packet.Type])
+		}
+		if !supportsBinary {
+			b64 := base64.NewEncoder(base64.StdEncoding, encode)
+			defer b64.Close()
+
+			v.WriteTo(b64)
+		} else {
+			v.WriteTo(encode)
+		}
 	case *bytes.Reader:
-		buf := new(bytes.Buffer)
-		if _, err := buf.ReadFrom(v); err != nil {
-			return nil, err
+		if !supportsBinary {
+			encode.Write([]byte{'b', packets[packet.Type]})
+		} else {
+			encode.WriteByte(packets[packet.Type])
 		}
-		dataByte = buf.Bytes()
+		if !supportsBinary {
+			b64 := base64.NewEncoder(base64.StdEncoding, encode)
+			defer b64.Close()
+
+			v.WriteTo(b64)
+		} else {
+			v.WriteTo(encode)
+		}
 	case *strings.Reader:
-		buf := new(bytes.Buffer)
-		if _, err := buf.ReadFrom(v); err != nil {
-			return nil, err
-		}
-		dataByte = Utf8encodeByte(buf.Bytes(), nil)
+		encode.WriteByte(packets[packet.Type])
+		v.WriteTo(encode)
 	default:
-	}
-
-	if !supportsBinary {
-		b64 := base64.NewEncoder(base64.StdEncoding, encode)
-		defer b64.Close()
-
-		b64.Write(dataByte)
-	} else {
-		encode.Write(dataByte)
 	}
 
 	return encode, nil
@@ -95,54 +98,111 @@ func EncodePacket(packet types.Packet, supportsBinary bool, utf8encode bool) (*b
  * Decodes a packet. Data also available as an ArrayBuffer if requested.
  *
  * @return {Object} with `type` and `data` (if any)
- * @api private
+ * @api public
  */
 
-// exports.decodePacket = function (data *bytes.Buffer, binaryType string, utf8decode bool) {
-//   if (data === undefined) {
-//     return err;
+// func DecodePacket(data io.Reader, binaryType string, utf8decode bool) (*types.Packet, error) {
+// 	if data == nil {
+// 		return err
+// 	}
+// 	if c, ok := data.(io.Closer); ok {
+// 		defer c.Close()
+// 	}
+// 	msgType := []byte{0XFF}
+// 	decode := bytes.NewBuffer(nil)
+
+// 	switch v := data.(type) {
+// 	case *bytes.Buffer:
+// 		dataByte = v.Bytes()
+// 	case *bytes.Reader:
+// 		buf := new(bytes.Buffer)
+// 		if _, err := buf.ReadFrom(v); err != nil {
+// 			return nil, err
+// 		}
+// 		dataByte = buf.Bytes()
+// 	case *strings.Reader:
+// 		if _, err := v.Read(msgType); err != nil {
+// 			return nil, err
+// 		}
+// 		if msgType[0] == 'b' {
+// 			if _, err := v.Read(msgType); err != nil {
+// 				return nil, err
+// 			}
+// 			Type := packetslist[msgType[0]]
+// 			buf := new(bytes.Buffer)
+// 			if _, err := buf.ReadFrom(v); err != nil {
+// 				return nil, err
+// 			}
+// 			b64 = base64.NewDecoder(base64.StdEncoding, decode)
+// 			defer b64.Close()
+// 			b64.Write(buf.Bytes())
+// 			return types.Packet{
+// 				Type: Type,
+// 				Data: decode,
+// 			}
+// 		}
+// 		if utf8decode {
+// 			data := tryDecode(data)
+// 			if data == false {
+// 				return err
+// 			}
+// 		}
+// 	// buf := new(bytes.Buffer)
+// 	// if _, err := buf.ReadFrom(v); err != nil {
+// 	// 	return nil, err
+// 	// }
+// 	// dataByte = Utf8encodeByte(buf.Bytes(), &Opts{Strict: false})
+// 	default:
+// 	}
+// 	//   var type;
+
+// 	// String data
+// 	//   if (typeof data === 'string') {
+
+// 	//     type = data.charAt(0);
+
+// 	//     if (type === 'b') {
+// 	//       return exports.decodeBase64Packet(data.substr(1), binaryType);
+// 	//     }
+
+// 	//     if (utf8decode) {
+// 	//       data = tryDecode(data);
+// 	//       if (data === false) {
+// 	//         return err;
+// 	//       }
+// 	//     }
+
+// 	//     if (Number(type) != type || !packetslist[type]) {
+// 	//       return err;
+// 	//     }
+
+// 	//     if (data.length > 1) {
+// 	//       return { type: packetslist[type], data: data.substring(1) };
+// 	//     } else {
+// 	//       return { type: packetslist[type] };
+// 	//     }
+// 	//   }
+
+// 	//   // Binary data
+// 	//   if (binaryType === 'arraybuffer') {
+// 	//     // wrap Buffer/ArrayBuffer data into an Uint8Array
+// 	//     var intArray = new Uint8Array(data);
+// 	//     type = intArray[0];
+// 	//     return { type: packetslist[type], data: intArray.buffer.slice(1) };
+// 	//   }
+
+// 	//   if (data instanceof ArrayBuffer) {
+// 	//     data = arrayBufferToBuffer(data);
+// 	//   }
+// 	//   type = data[0];
+// 	//   return { type: packetslist[type], data: data.slice(1) };
+// }
+
+// func tryDecode(data ) {
+//   try {
+//     data = utf8.decode(data, { strict: false });
+//   } catch (e) {
+//     return false;
 //   }
-
-//   var type;
-
-//   // String data
-//   if (typeof data === 'string') {
-
-//     type = data.charAt(0);
-
-//     if (type === 'b') {
-//       return exports.decodeBase64Packet(data.substr(1), binaryType);
-//     }
-
-//     if (utf8decode) {
-//       data = tryDecode(data);
-//       if (data === false) {
-//         return err;
-//       }
-//     }
-
-//     if (Number(type) != type || !packetslist[type]) {
-//       return err;
-//     }
-
-//     if (data.length > 1) {
-//       return { type: packetslist[type], data: data.substring(1) };
-//     } else {
-//       return { type: packetslist[type] };
-//     }
-//   }
-
-//   // Binary data
-//   if (binaryType === 'arraybuffer') {
-//     // wrap Buffer/ArrayBuffer data into an Uint8Array
-//     var intArray = new Uint8Array(data);
-//     type = intArray[0];
-//     return { type: packetslist[type], data: intArray.buffer.slice(1) };
-//   }
-
-//   if (data instanceof ArrayBuffer) {
-//     data = arrayBufferToBuffer(data);
-//   }
-//   type = data[0];
-//   return { type: packetslist[type], data: data.slice(1) };
-// };
+//   return data;
+// }

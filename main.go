f8e3d5654270501efda11e3bdcdcdc92cc66121e
zishang520 <zishang520@gmail.com>
2019-12-02 14:28:26 +0800

    fixbug

--- main.go
+++ main.go
@@ -19,10 +19,10 @@ func main() {
 			Type: "ping",
 			Data: strings.NewReader(`[]byte{0, 1你好呀, 2, 3, 4, 5, 6, 7, 8, 9}b2W11ieXRlezAsIDHkvaDlpb3lkYAsIDIsIDMsIDQsIDUsIDYsIDcsIDgsIDl9`),
 		},
-		// &types.Packet{
-		// 	Type: "noop",
-		// 	Data: strings.NewReader(`[]byte{0, 1你好呀, 8, 9}b2W11ieXRlezAsIDHkvaDlpb3lkYAsIDIsIDMsIDQsIDUsIDYsIDcsIDgsIDl9`),
-		// },
+		&types.Packet{
+			Type: "noop",
+			Data: strings.NewReader(`[]byte{0, 1你好呀, 8, 9}b2W11ieXRlezAsIDHkvaDlpb3lkYAsIDIsIDMsIDQsIDUsIDYsIDcsIDgsIDl9`),
+		},
 	}, true)
 	fmt.Println(err)
 	fmt.Println(hex.EncodeToString(buf.Bytes()))

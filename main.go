da59d55dc8d027647e2696f667b0b33d77e60d96
zishang520 <zishang520@gmail.com>
2019-12-16 14:23:56 +0800

    back

--- main.go
+++ main.go
@@ -43,13 +43,12 @@ func main() {
 		return true
 	}))
 
-	boolss := parser.DecodePayload(strings.NewReader(`102:2[]byte{0, 1你好呀, 2, 3, 4, 5, 6, 7, 8, 9}b2W11ieXRlezAsIDHkvaDlpb3lkYAsIDIsIDMsIDQsIDUsIDYsIDcsIDgsIDl9103:2[]by:te{0, 1你好呀, 2, 3, 4, 5, 6, 7, 8, 9}b2W11ieXRlezAsIDHkvaDlpb3lkYAsIDIsIDMsIDQsIDUsIDYsIDcsIDgsIDl987:6[]by:te{0, 1你好呀, 8, 9}b2W11ieXRlezAsIDHkvaDlpb3lkYAsIDIsIDMsIDQsIDUsIDYsIDcsIDgsIDl9`), func(a *types.Packet, b int, c int) bool {
+	fmt.Println(parser.DecodePayload(strings.NewReader(`35:2102:2[]byte{0, 1你好呀, 2, 3, 4, 5, 614:b1eHh4eHh4eA==1:61:6`), func(a *types.Packet, b int, c int) bool {
 		fmt.Println(a)
 		fmt.Println(b)
 		fmt.Println(c)
 		return true
-	})
-	fmt.Println(boolss)
+	}))
 
 	SignalC := make(chan os.Signal)
 

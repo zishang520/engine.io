d7bedeb0d88ca6ce1be0a9c1533b659fd87b46d2
luoyy <zishang520@gmail.com>
2021-04-22 19:55:06 +0800

    预添加

--- types/opts.go
+++ types/opts.go
@@ -14,25 +14,45 @@ type Cookie struct {
 }
 
 type Opts struct {
-	WsEngine          string            `json:"wsEngine"`
-	PingTimeout       time.Duration     `json:"pingTimeout"`
-	PingInterval      time.Duration     `json:"pingInterval"`
-	UpgradeTimeout    time.Duration     `json:"upgradeTimeout"`
-	MaxHttpBufferSize time.Duration     `json:"maxHttpBufferSize"`
-	Transports        Set               `json:"transports"`
-	AllowUpgrades     bool              `json:"allowUpgrades"`
-	AllowRequest      AllowRequest      `json:"allowRequest"`
-	Cookie            Cookie            `json:"cookie"`
-	CookiePath        string            `json:"cookiePath"`
-	CookieHttpOnly    string            `json:"cookieHttpOnly"`
-	PerMessageDeflate PerMessageDeflate `json:"perMessageDeflate"`
-	HttpCompression   HttpCompression   `json:"httpCompression"`
-	Cors              bool              `json:"cors"`
-	AllowEIO3         bool              `json:"allowEIO3"`
+	WsEngine          string             `json:"wsEngine"`
+	PingTimeout       time.Duration      `json:"pingTimeout"`
+	PingInterval      time.Duration      `json:"pingInterval"`
+	UpgradeTimeout    time.Duration      `json:"upgradeTimeout"`
+	MaxHttpBufferSize time.Duration      `json:"maxHttpBufferSize"`
+	Transports        Set                `json:"transports"`
+	AllowUpgrades     bool               `json:"allowUpgrades"`
+	AllowRequest      AllowRequest       `json:"allowRequest"`
+	Cookie            *Cookie            `json:"cookie"`
+	PerMessageDeflate *PerMessageDeflate `json:"perMessageDeflate"`
+	HttpCompression   *HttpCompression   `json:"httpCompression"`
+	Cors              bool               `json:"cors"`
+	AllowEIO3         bool               `json:"allowEIO3"`
 }
 
 func OptsInit() *Opts {
-	return &Opts{}
+	return &Opts{
+		// WsEngine: DEFAULT_WS_ENGINE,
+		PingTimeout:       20000,
+		PingInterval:      25000,
+		UpgradeTimeout:    10000,
+		MaxHttpBufferSize: 1e6,
+		Transports:        Set{"polling": NULL, "websocket": NULL},
+		AllowUpgrades:     true,
+		Cookie: &Cookie{
+			Name:     "io",
+			Path:     "/",
+			HttpOnly: true,
+			SameSite: "lax",
+		},
+		PerMessageDeflate: &PerMessageDeflate{
+			Threshold: 1024,
+		},
+		HttpCompression: &HttpCompression{
+			Threshold: 1024,
+		},
+		Cors:      false,
+		AllowEIO3: false,
+	}
 }
 
 func (o *Opts) Assign() {

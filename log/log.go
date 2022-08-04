ca8663596eb6cc48b42292c8bc7a5114ba05c5a4
Luoyy <zishang520@gmail.com>
2022-08-04 11:31:01 +0800

    Add log debug environment variable filtering

---
+++ log/log.go
@@ -0,0 +1,106 @@
+package log
+
+import (
+	"github.com/gookit/color"
+	_log "log"
+	"os"
+	"regexp"
+	"strings"
+	"sync"
+)
+
+type Log struct {
+	*_log.Logger
+
+	DEBUG bool
+
+	mu              sync.Mutex // ensures atomic writes; protects the following fields
+	prefix          string
+	namespaceRegexp *regexp.Regexp
+}
+
+func NewLog() *Log {
+	l := &Log{
+		Logger: _log.New(os.Stderr, "", 0),
+		DEBUG:  false,
+	}
+	if debug := os.Getenv("DEBUG"); debug != "" {
+		l.namespaceRegexp = regexp.MustCompile(strings.ReplaceAll(regexp.QuoteMeta(strings.TrimSpace(debug)), `\*`, `.*`))
+	}
+	return l
+}
+
+func (d *Log) checkNamespace(namespace string) bool {
+	if d.namespaceRegexp != nil {
+		return d.namespaceRegexp.MatchString(namespace)
+	}
+	return false
+}
+
+// Console log Println.
+func (d *Log) Println(message string, args ...interface{}) {
+	d.Logger.Println(color.Sprintf(message, args...))
+}
+
+// Console log Default.
+func (d *Log) Default(message string, args ...interface{}) {
+	d.Logger.Println(color.Tag("default").Sprintf(message, args...))
+}
+
+// Console log Info.
+func (d *Log) Info(message string, args ...interface{}) {
+	d.Logger.Println(color.Info.Sprintf(message, args...))
+}
+
+// Console Debug Debug.
+func (d *Log) Debug(message string, args ...interface{}) {
+	if d.DEBUG || d.checkNamespace(d.Prefix()) {
+		d.Logger.Println(color.Debug.Sprintf(message, args...))
+	}
+}
+
+// Console log Success.
+func (d *Log) Success(message string, args ...interface{}) {
+	d.Logger.Println(color.Success.Sprintf(message, args...))
+}
+
+// Console log Error.
+func (d *Log) Error(message string, args ...interface{}) {
+	d.Logger.Println(color.Danger.Sprintf(message, args...))
+}
+
+// Console log Warning.
+func (d *Log) Warning(message string, args ...interface{}) {
+	d.Logger.Println(color.Warn.Sprintf(message, args...))
+}
+
+// Console log Secondary.
+func (d *Log) Secondary(message string, args ...interface{}) {
+	d.Logger.Println(color.Secondary.Sprintf(message, args...))
+}
+
+// Console log Secondary.
+func (d *Log) Question(message string, args ...interface{}) {
+	d.Logger.Println(color.Question.Sprintf(message, args...))
+}
+
+// Console log Fatal.
+func (d *Log) Fatal(message string, args ...interface{}) {
+	d.Logger.Fatal(color.Error.Sprintf(message, args...))
+}
+
+// Prefix returns the output prefix for the logger.
+func (d *Log) Prefix() string {
+	d.mu.Lock()
+	defer d.mu.Unlock()
+	return d.prefix
+}
+
+// SetPrefix sets the output prefix for the logger.
+func (d *Log) SetPrefix(prefix string) {
+	d.mu.Lock()
+	defer d.mu.Unlock()
+	d.prefix = prefix
+
+	d.Logger.SetPrefix(prefix + " ")
+}
--- utils/log.go
+++ utils/log.go
@@ -1,69 +1,16 @@
 package utils
 
 import (
-	"github.com/gookit/color"
-	"log"
+	"github.com/zishang520/engine.io/log"
 )
 
-type Debug struct {
-	DEBUG bool
-	Log   *log.Logger
+var _log = log.NewLog()
+
+func init() {
+	_log.SetFlags(0)
+	_log.SetPrefix("engine")
 }
 
-var _log = &Debug{DEBUG: false, Log: log.Default()}
-
-func Log() *Debug {
+func Log() *log.Log {
 	return _log
-}
-
-// Console log Println.
-func (d *Debug) Println(message string, args ...interface{}) {
-	d.Log.Println(color.Sprintf(message, args...))
-}
-
-// Console log Default.
-func (d *Debug) Default(message string, args ...interface{}) {
-	d.Log.Println(color.Tag("default").Sprintf(message, args...))
-}
-
-// Console log Info.
-func (d *Debug) Info(message string, args ...interface{}) {
-	d.Log.Println(color.Info.Sprintf(message, args...))
-}
-
-// Console Debug Debug.
-func (d *Debug) Debug(message string, args ...interface{}) {
-	if d.DEBUG {
-		d.Log.Println(color.Debug.Sprintf(message, args...))
-	}
-}
-
-// Console log Success.
-func (d *Debug) Success(message string, args ...interface{}) {
-	d.Log.Println(color.Success.Sprintf(message, args...))
-}
-
-// Console log Error.
-func (d *Debug) Error(message string, args ...interface{}) {
-	d.Log.Println(color.Danger.Sprintf(message, args...))
-}
-
-// Console log Warning.
-func (d *Debug) Warning(message string, args ...interface{}) {
-	d.Log.Println(color.Warn.Sprintf(message, args...))
-}
-
-// Console log Secondary.
-func (d *Debug) Secondary(message string, args ...interface{}) {
-	d.Log.Println(color.Secondary.Sprintf(message, args...))
-}
-
-// Console log Secondary.
-func (d *Debug) Question(message string, args ...interface{}) {
-	d.Log.Println(color.Question.Sprintf(message, args...))
-}
-
-// Console log Fatal.
-func (d *Debug) Fatal(message string, args ...interface{}) {
-	d.Log.Fatal(color.Error.Sprintf(message, args...))
 }

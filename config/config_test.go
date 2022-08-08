6d1c57e3f25358febda13e64b624884f69889942
Luoyy <zishang520@gmail.com>
2022-08-08 18:01:09 +0800

    add some unit tests

---
+++ config/config_test.go
@@ -0,0 +1,85 @@
+package config
+
+import (
+	"testing"
+	"time"
+)
+
+func TestAttachOptionsDefauleValue(t *testing.T) {
+	opts := &AttachOptions{}
+	t.Run("path", func(t *testing.T) {
+		if path := opts.Path(); opts.path == nil && path != "/engine.io" {
+			t.Fatalf(`AttachOptions.Path() = %q, want match for %#q`, path, "/engine.io")
+		}
+	})
+
+	t.Run("destroyUpgrade", func(t *testing.T) {
+		if destroyUpgrade := opts.DestroyUpgrade(); opts.destroyUpgrade == nil && destroyUpgrade != true {
+			t.Fatalf(`AttachOptions.DestroyUpgrade() = %t, want match for %t`, destroyUpgrade, true)
+		}
+	})
+
+	t.Run("destroyUpgradeTimeout", func(t *testing.T) {
+		if destroyUpgradeTimeout := opts.DestroyUpgradeTimeout(); opts.destroyUpgradeTimeout == nil && destroyUpgradeTimeout != 1000*time.Millisecond {
+			t.Fatalf(`AttachOptions.DestroyUpgradeTimeout() = %d, want match for %d`, destroyUpgradeTimeout, 1000*time.Millisecond)
+		}
+	})
+}
+
+func TestServerOptionsDefauleValue(t *testing.T) {
+	opts := &ServerOptions{}
+
+	t.Run("pingTimeout", func(t *testing.T) {
+		if pingTimeout := opts.PingTimeout(); opts.pingTimeout == nil && pingTimeout != 20000*time.Millisecond {
+			t.Fatalf(`ServerOptions.PingTimeout() = %d, want match for %d`, pingTimeout, 20000*time.Millisecond)
+		}
+	})
+
+	t.Run("pingInterval", func(t *testing.T) {
+		if pingInterval := opts.PingInterval(); opts.pingInterval == nil && pingInterval != 25000*time.Millisecond {
+			t.Fatalf(`ServerOptions.PingInterval() = %d, want match for %d`, pingInterval, 25000*time.Millisecond)
+		}
+	})
+
+	t.Run("upgradeTimeout", func(t *testing.T) {
+		if upgradeTimeout := opts.UpgradeTimeout(); opts.upgradeTimeout == nil && upgradeTimeout != 10000*time.Millisecond {
+			t.Fatalf(`ServerOptions.UpgradeTimeout() = %d, want match for %d`, upgradeTimeout, 10000*time.Millisecond)
+		}
+	})
+
+	t.Run("maxHttpBufferSize", func(t *testing.T) {
+		if maxHttpBufferSize := opts.MaxHttpBufferSize(); opts.maxHttpBufferSize == nil && maxHttpBufferSize != 100000 {
+			t.Fatalf(`ServerOptions.MaxHttpBufferSize() = %d, want match for %d`, maxHttpBufferSize, 100000)
+		}
+	})
+
+	t.Run("allowRequest", func(t *testing.T) {
+		if allowRequest := opts.AllowRequest(); opts.allowRequest == nil && allowRequest != nil {
+			t.Fatalf(`ServerOptions.AllowRequest() = %v, want match for nil`, allowRequest)
+		}
+	})
+
+	t.Run("transports", func(t *testing.T) {
+		if transports := opts.Transports(); opts.transports == nil && transports != nil && !(transports.Has("polling") && transports.Has("websocket")) {
+			t.Fatalf(`ServerOptions.Transports() = %s, want match for ["polling", "websocket")]`, transports.Keys())
+		}
+	})
+
+	t.Run("allowUpgrades", func(t *testing.T) {
+		if allowUpgrades := opts.AllowUpgrades(); opts.allowUpgrades == nil && allowUpgrades != true {
+			t.Fatalf(`ServerOptions.AllowUpgrades() = %t, want match for %t`, allowUpgrades, true)
+		}
+	})
+
+	t.Run("perMessageDeflate", func(t *testing.T) {
+		if perMessageDeflate := opts.PerMessageDeflate(); opts.perMessageDeflate == nil && perMessageDeflate != nil {
+			t.Fatalf(`ServerOptions.PerMessageDeflate() = %v, want match for nil`, perMessageDeflate)
+		}
+	})
+
+	t.Run("httpCompression/threshold", func(t *testing.T) {
+		if httpCompression := opts.HttpCompression(); opts.httpCompression == nil && httpCompression != nil && httpCompression.Threshold != 1024 {
+			t.Fatalf(`ServerOptions.HttpCompression().Threshold = %d, want match for %d`, httpCompression.Threshold, 1024)
+		}
+	})
+}
--- go.mod
+++ go.mod
@@ -3,12 +3,17 @@ module github.com/zishang520/engine.io
 go 1.18
 
 retract (
-    v1.0.0
-    v1.0.1
+	v1.0.1
+	v1.0.0
 )
 
 require (
 	github.com/andybalholm/brotli v1.0.4
 	github.com/gookit/color v1.5.0
 	github.com/gorilla/websocket v1.5.0
+)
+
+require (
+	github.com/xo/terminfo v0.0.0-20210125001918-ca9a967f8778 // indirect
+	golang.org/x/sys v0.0.0-20210330210617-4fbd30eecc44 // indirect
 )
